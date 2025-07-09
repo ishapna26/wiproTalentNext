879393eb41ff48910854df0f9c46d79cbca85db0
SHAPNA <160203414+ishapna26@users.noreply.github.com>
2025-07-09 10:25:47 +0530

    Update code.java

--- code.java
+++ code.java
@@ -16,6 +16,31 @@ class Main3{
 		System.out.print(a.size());
 	}
 }
+(or)
+import java.util.*;
+
+class Main3{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int[] a=new int[10];
+		
+		while(n>0) {
+			int mod=n%10;
+			if(a[mod]==0) {
+				a[mod]=1;
+			}
+			n=n/10;
+		}
+		int c=0;
+		for(int i=0;i<10;i++) {
+			if(a[i]==1) {
+				c++;
+			}
+		}
+		System.out.print(c);
+	}
+}
 
 //find the number of non repeated elements
 import java.util.*;
