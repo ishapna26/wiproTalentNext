40aca24a252ca720b03ad83fc561e9d4605560e2
SHAPNA <160203414+ishapna26@users.noreply.github.com>
2025-07-15 00:15:08 +0530

    Update code2.java

--- code2.java
+++ code2.java
@@ -33,3 +33,29 @@ class Main3{
 		}
 	}
 }
+
+2. String Palindrome
+import java.util.*;
+
+class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String str=sc.next();
+        String str1=str.toLowerCase();
+        String original=str1;
+        String str2="";
+        
+        int len=str1.length();
+        
+        for(int i=len-1;i>=0;i--){
+            str2=str2+""+str1.charAt(i);
+        }
+        
+        if(str1.equals(str2)){
+            System.out.print("palindrome");
+        }
+        else{
+            System.out.print("non palindrome");
+        }
+    }
+}
