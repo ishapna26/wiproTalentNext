622552b759ec87a7b96db84f8bf5c15fd7e3a8fe
SHAPNA <160203414+ishapna26@users.noreply.github.com>
2025-07-16 12:33:22 +0530

    Create WrapperClass.java

---
+++ WrapperClass.java
@@ -0,0 +1,17 @@
+Every class in java extends java object class
+In java there are primitive data types. Hence these datatypes makes java 99.99℅ object oriented. Since it is not an object
+Collection is similar to array
+It is a object type where as array is object type
+Wrapper class
+That why java has wrapper class
+For every primitive type we have class format
+For eg we can declare a  integer in two ways
+int n=7 or
+Integer num=new Integer (value) //this will geneate an warning because we are assigning an int to the class object which is called autoboxing. 
+Integer num=8 // in this case the int value is converted in to class automatically. So this is called autoboxing
+
+How to convert the object into int
+int num=7
+Integer n = num
+int n2=n.intValue() // this is called unboxing
+Int n2=n //auto unboxing
