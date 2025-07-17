1bcc983b12170cfec305899ebe8fbb07ed682cb6
SHAPNA <160203414+ishapna26@users.noreply.github.com>
2025-07-17 10:17:16 +0530

    Update WrapperClass.java

--- WrapperClass.java
+++ WrapperClass.java
@@ -3,7 +3,7 @@ x Every class in java extends java object class
 In java there are primitive data types. Hence these datatypes makes java 99.99℅ object oriented. Since it is not an object Collection is similar to array
 It is a object type where as array is object type
   
-Wrapper class:
+Wrapper class: It is found in the java.lang package which gets imported automatically when you create a java file.
 -------------
 Thats why java has wrapper class
 For every primitive type we have class format
